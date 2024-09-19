568a14f194da9d84ba8e0e4d932a6767814f7a66
nyhp-2020 <63298811+nyhp-2020@users.noreply.github.com>
2024-09-19 14:51:48 +0200

    Delete scripts/scripts2 directory

--- scripts/scripts2/windowing.sql
+++
@@ -1,8 +0,0 @@
-select 
-    *, 
-    lag(OrderDate, 1, OrderDate) over(partition by CustomerID order by OrderDate) as [LAG],
-    lead(OrderDate, 1, OrderDate) over(partition by CustomerID order by OrderDate) as [LEAD],
-    first_value(SubTotal) over(partition by CustomerID order by (select null))
-from Orders
---where CustomerID = 29734
-order by CustomerID, OrderDate
