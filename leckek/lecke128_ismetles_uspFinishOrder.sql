97af64086ef65b502fbdd25d8b5263328df81a75
nyhp-2020 <63298811+nyhp-2020@users.noreply.github.com>
2025-02-12 10:18:57 +0100

    Add files via upload

--- leckek/lecke128_ismetles_uspFinishOrder.sql
+++ leckek/lecke128_ismetles_uspFinishOrder.sql
@@ -28,6 +28,10 @@ set i.ProductLeft = i.ProductLeft - po.OrderQuantity
 from dbo.ProductOrders po
 inner join dbo.Inventory i on i.ID = po.InventoryID
 where po.OrderID = @OrderID
+/*
+Ha a ProductOrders-ben benne vannak uspAddToOrder által rosszul beszúrt plusz sorok (Több ProductID az Inventory-ban),
+akkor itt mindegyiket módosítja (mindegyikbõl kivonja a mennyiséget)
+*/
 
 --törlöm a zárolásokat
 delete
@@ -38,6 +42,15 @@ where OrderID = @OrderID
 --select * from  dbo.Inventory where ProductID = 1003
 --select * from dbo.ProductOrders where OrderID = @OrderID
 go
+
+/*
+Itt is kérdés, hogy a dbo.Inventory-ban megengedünk-e több sort ugyanazzal a ProductID-vel.
+Mert az uspFinishOrder eljárás több sort is fog módosítani az Inventory-ban.
+És több sort fog törölni a dbo.ProductOrders-bõl,
+amit korábban az uspAddToOrder eljárás szúrt be oda ugyanazzal az OrderID-vel és OrderQuantity-vel,
+pont az miatt hogy a dbo.Inventory-ban több sor van ugyanazzal a ProductID-vel...
+Vagy kényszerítsük ki, hogy a dbo.Inventory-ban a ProductID egyedi legyen?
+*/
 
 select * from dbo.Orders where OrderID = 11
 select * from  dbo.Inventory where ProductID = 1003
