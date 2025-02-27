36acb9ae9af069ea5b9744f51a420e2a63545c5a
nyhp-2020 <63298811+nyhp-2020@users.noreply.github.com>
2025-02-27 10:41:11 +0100

    Add files via upload

--- leckek/lecke183_connecting.sql
+++ leckek/lecke183_connecting.sql
@@ -3,8 +3,10 @@ from Inventory i
 inner join Product p on i.ProductID = p.ProductID
 where i.ProductLeft > 0
 
+SELECT * FROM Inventory
+WHERE ProductLeft > 0
 
 exec uspAddToInventory
 @ProductID
-@Uantity
+@Quantity
 @Shelf
